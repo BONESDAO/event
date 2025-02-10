45e47b2186544447d362d499f9f276272158b234
XZC <a18970250192@163.com>
2025-02-10 19:56:57 +0800

    upx

--- next.config.ts
+++ next.config.ts
@@ -5,6 +5,10 @@ const nextConfig: NextConfig = {
     locales: ['en', 'zh'], 
     defaultLocale: 'zh',
   },
+  serverRuntimeConfig: {
+    // 设置一个较高的构建超时，单位是毫秒
+    buildTimeout: 240000, // 默认是 60秒
+  },
 };
 
 export default nextConfig;
